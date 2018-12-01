ae6372b6948776c86457679100c9d460f43e1933
warrendevonshire <devonshiw5@students.rowan.edu>
2018-12-01 11:32:54 -0500

    Created Test file for the RSA Class.

---
+++ tests/RSATest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RSATest {
+
+    @Test
+    void inverse() {
+    }
+
+    @Test
+    void modPower() {
+    }
+}
